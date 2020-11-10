109aa7794ff3cc34759c04828ae593b4bc39d6c3
Giannis Koutsoukos <iokouts@gmail.com>
2020-11-10 22:45:01 +0200

    added socket ping message

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -4,18 +4,32 @@ import {AuthContext} from 'services/auth';
 
 const useSocket = () => {
   const {username} = React.useContext(AuthContext);
+  const pingMessage = {
+    command: 3,
+    channel: 'general',
+    content: 'ping',
+    user: username
+  }
   const {
     sendJsonMessage,
     lastJsonMessage,
     readyState,
     getWebSocket
-  } = useWebSocket(
-    `ws://${process.env.REACT_APP_API_URL}/chat?username=${username}`,
-    {share: true}
+  } = useWebSocket(`ws://${process.env.REACT_APP_API_URL}/chat?username=${username}`, {
+    share: true,
+    onError: (error) => {
+      console.log('socket error' + error.type)
+      getWebSocket().close()
+    },
+    onClose: (msg) => console.log('socket closed. Message: ' + msg.reason + ' Code: ' + msg.code )
+  }
   );
 
   React.useEffect(() => () => getWebSocket().close(), [getWebSocket])
-  
+  React.useEffect(() => {
+    const keepAlive = setInterval(() => sendJsonMessage(pingMessage), 50000);
+    return () => clearInterval(keepAlive);
+  }, []);
   return {
     sendJsonMessage,
     lastJsonMessage,

24f6c3da9efc4f6ee03fc16b9f5f48b5a342177b
Odysseas Zafeiriou <odysseas@wearereasonablepeople.com>
2021-02-21 21:31:31 +0100

    Fix play pause; add volume

--- src/components/organisms/Player.tsx
+++ src/components/organisms/Player.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 import PlayRoundedIcon from '@material-ui/icons/PlayArrowRounded';
-import PauseRoundedIcon from '@material-ui/icons/PauseRounded';
+import StopRoundedIcon from '@material-ui/icons/StopRounded';
+import Slider from '@material-ui/core/Slider';
+import VolumeDown from '@material-ui/icons/VolumeDown';
+import VolumeUp from '@material-ui/icons/VolumeUp';
 import {palette} from 'styles/theme';
 import {Button, Typography} from '@material-ui/core';
 import {StreamContext} from 'services/stream';
+import {clamp} from 'ramda';
 
 const PlayerContainer = styled.div`
   margin-bottom: 24px;
@@ -52,7 +56,7 @@ const PlayIcon = styled(PlayRoundedIcon)`
   }
 `;
 
-const PauseIcon = styled(PauseRoundedIcon)`
+const StopIcon = styled(StopRoundedIcon)`
   color: ${palette.yellow};
   font-size: 44px !important;
   @media(min-width: 960px) {
@@ -69,34 +73,48 @@ const CurrentSongInfo = styled.div`
   justify-content: center;
 `;
 
+const streamURL = "https://radio-admin.kickit.gr/radio/8000/radio.mp3";
+
 const Player: React.FC = () => {
   const {currentSong: {songArtist, songTitle}} = React.useContext(StreamContext);
-  const playerRef = React.useRef(document.createElement('audio'));
+  const playerRef = React.useRef<HTMLAudioElement>(null);
   const [paused, setPaused] = React.useState<boolean>(true);
+  const [volume, setVolume] = React.useState<number>(100);
   React.useEffect(() => {
-    setPaused(false);
-    playerRef.current.play();
-  }, [])
+    if (playerRef.current) {
+      playerRef.current.volume = clamp(volume / 100, 1, 0)
+    }
+  }, [volume])
   return (
     <PlayerContainer>
       <PlayPauseButton
         onClick={() => {
           setPaused(!paused);
-          paused
-            ? playerRef.current.play()
-            : playerRef.current.pause()
+          if (playerRef.current) {
+            if (paused) {
+              playerRef.current.src = streamURL;
+              playerRef.current.play();
+            } else {
+              playerRef.current.src = '';
+              playerRef.current.pause();
+            }
+          }
         }}
       >
-        {paused ? <PlayIcon /> : <PauseIcon />}
+        {paused ? <PlayIcon /> : <StopIcon />}
       </PlayPauseButton>
+      <VolumeDown />
+      <Slider
+        value={volume}
+        onChange={(event: any, newValue: number | number[]) => setVolume(newValue as number)}
+        aria-labelledby="continuous-slider"
+      />
+      <VolumeUp />
       <CurrentSongInfo>
         <Typography variant="h6" style={{fontWeight: 'bold'}}>{songTitle}</Typography>
         <Typography variant="body1" >{songArtist}</Typography>
       </CurrentSongInfo>
-      <audio
-        ref={playerRef}
-        src='http://radio.kickit.gr/radio/8000/radio.mp3'
-      />
+      <audio ref={playerRef} />
     </PlayerContainer>
   )
 }
